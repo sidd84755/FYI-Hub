412832625d7b669fb869ac6e387a030b65e452d5
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-05-17 23:10:05 +0530

    Initial commit

---
+++ FYI Hub/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+import { Box } from "@mui/material"
+
+const App = () => {
+  return (
+    <Box>
+      <Navbar/>
+      hi there
+    </Box>
+  )
+}
+
+export default App
